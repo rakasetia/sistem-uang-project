72b83cf6a4cb9f3c21587ec68303802cf1384556
rakasetia <rakasetiyawan65@gmail.com>
2024-12-13 13:53:35 +0700

    er

--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -3,7 +3,7 @@
 
 <head>
 
-    <title>Uangku</title>
+    <title>menejemen keuangan santri</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
